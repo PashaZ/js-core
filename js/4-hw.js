222d88bec388ca87be853ed5f171a16d872be917
Pasha1 <pavlo.zakernychnyy@gmail.com>
2021-04-12 16:12:40 +0300

    Homework5  +  HW4 - task4

--- js/4-hw.js
+++ js/4-hw.js
@@ -1,104 +1,106 @@
-task1
+// task1
 
-function calcRectangleArea(width, height){
-  if (Number(width) && Number(height)){
-let square = width * height ;
-console.log(square);
-  }
-  else {
-    throw new Error("нечислове значення");
-  }
-}
-try {
-  calcRectangleArea(2, 5);
-}
- catch (exception) {
-  console.log(exception.name);
-  console.log(exception.message);
-}
-
-
-
-// task2
-
-function checkAge() {
-
-  let age = prompt("vvedit svij vik");
-  if (age.length == 0) {
-    throw new Error("The field is empty! Please enter your age");
-  }
-  else {
-    let ageNew = Number(age);
-    if (ageNew < 14) {
-      throw new Error("вік юзера менше 14 років");
-    }
-    else if (typeof ageNew == "number" && ageNew >= 14) {
-      console.log("dostup dozvoleno");
-    }
-    else {
-      throw new Error("нечислове значення");
-    }
-  }
-}
-  try {
-    checkAge();
-  }
-  catch (ex) {
-    console.log(ex.name);
-    console.log(ex.message);
-  }
+// function calcRectangleArea(width, height){
+//   if (Number(width) && Number(height)){
+// let square = width * height ;
+// console.log(square);
+//   }
+//   else {
+//     throw new Error("нечислове значення");
+//   }
+// }
+// try {
+//   calcRectangleArea(2, 5);
+// }
+//  catch (exception) {
+//   console.log(exception.name);
+//   console.log(exception.message);
+// }
 
 
-// task3
 
-class MonthException {
-  message = "MonthException";
-  monthArr = [ "January","February", "March", "April",
-  "May","June","July","August", "September","October",
-  "November","December",];
-  constructor(constr) {
-    this.message;
-    this.monthArr[constr];
-  }
-}
-function showMonth(month) {
-  let m = new MonthException();
-  if (month < 1 || month > 12) {
-    throw new Error(m.message + " Incorect month number");
-  } else {
-    console.log(m.monthArr[month - 1]);
-  }
-}
-try {
-  showMonth(8);
-} catch (ex) {
-  console.log(ex.message);
-}
+// // task2
 
-// task4
+// function checkAge() {
+
+//   let age = prompt("vvedit svij vik");
+//   if (age.length == 0) {
+//     throw new Error("The field is empty! Please enter your age");
+//   }
+//   else {
+//     let ageNew = Number(age);
+//     if (ageNew < 14) {
+//       throw new Error("вік юзера менше 14 років");
+//     }
+//     else if (typeof ageNew == "number" && ageNew >= 14) {
+//       console.log("dostup dozvoleno");
+//     }
+//     else {
+//       throw new Error("нечислове значення");
+//     }
+//   }
+// }
+//   try {
+//     checkAge();
+//   }
+//   catch (ex) {
+//     console.log(ex.name);
+//     console.log(ex.message);
+//   }
+
+
+// // task3
+
+// class MonthException {
+//   message = "MonthException";
+//   monthArr = [ "January","February", "March", "April",
+//   "May","June","July","August", "September","October",
+//   "November","December",];
+//   constructor(constr) {
+//     this.message;
+//     this.monthArr[constr];
+//   }
+// }
+// function showMonth(month) {
+//   let m = new MonthException();
+//   if (month < 1 || month > 12) {
+//     throw new Error(m.message + " Incorect month number");
+//   } else {
+//     console.log(m.monthArr[month - 1]);
+//   }
+// }
+// try {
+//   showMonth(8);
+// } catch (ex) {
+//   console.log(ex.message);
+// }
+
+// // task4
 
 function showUser(id) {
   if (id < 0) {
     throw error();
   }
-  else {
-    return id;
-  }
+    return {id: id};
 }
-
 let ids = [7, -12, 44, 22, 50]
+
 function showUsers(ids) {
   let correct = [];
-  console.log(ids);
-  for (let i = 0; i < ids.length; i += 1) {
+
+//   console.log(ids);
+ids.forEach(function (id) {
     try {
-      showUser(ids[i]);
-      correct.push( ids[i]);
+
+      let person = showUser(id);
+      correct.push(person);
     }
     catch (error) {
-      console.log(" ID must not be negetive: " + ids[i]);
+      console.log(error + " ID must not be negetive: " + id);
     }
   }
+  );
   console.log(correct);
 }
+
 showUsers(ids);
--- js/5-hw.js
+++ js/5-hw.js
@@ -1,37 +1,262 @@
-// // task1
+// task1
 
-// function propsCount(currentObject) {
-//     console.log(Object.keys(currentObject).length);
-// };
+function propsCount(currentObject) {
+    console.log(Object.keys(currentObject).length);
+};
 
-// let currentObject = {
-//     course: "JS fundamental",
-//     duration: 3,
-//     direction: "web-development",
-//     kjkf: 5
-// };
+let currentObject = {
+    course: "JS fundamental",
+    duration: 3,
+    direction: "web-development",
+    kjkf: 5
+};
 
-// propsCount(currentObject);
+propsCount(currentObject);
 
-// // task2
+task2
+
+let firstObject = {
+
+    course: "JS fundamental",
+    duration: 3,
+    direction: "web-development",
+    payment: 7000,
+    certification: "yes"
+};
+
+function showProps(obj) {
+
+    let value = [];
+    for (let key in obj) {
+        value.push(obj[key]);
+    }
+
+    console.log(value);
+    console.log(Object.keys(obj));
+
+}
+
+showProps(firstObject);
 
 
-// function showProps(firstObject){
-    
-//     console.log(Object.keys(firstObject));
-//     console.log(Object.values(firstObject));
-// }
+// task 3
 
-// let firstObject ={
-  
-//     course: "JS fundamental", 
-//     duration: 3,
-//     direction: "web-development" ,
-//     payment: 7000,
-//     certification: "yes"
-// }
 
-// showProps(firstObject);
+class Person {
+    constructor(name, surname) {
+        this.name = name;
+        this.surname = surname;
+    }
+    showFullName() {
+        console.log(this.name + this.surname);
+    }
 
-// // task 3
+}
+class Student extends Person {
+    constructor(name, surname, year) {
+        super(name, surname);
+        this.year = year;
+    }
+
+    showFullName(midleName) {
+        return (this.surname + this.name + midleName);
+    }
+
+    showCourse() {
+        let year = new Date().getFullYear();
+        let currentCourse = year - this.year;
+        if (currentCourse > 6) {
+            currentCourse = "Graduated";
+        }
+        return currentCourse;
+    }
+}
+
+let stud1 = new Student("Pavlo ", "Zakernychnyy ", 2010)
+console.log(stud1.showFullName('Oleksandrovych')); 
+console.log('Current course: ' + stud1.showCourse()); 
+
+
+
+// task4
+
+
+
+console.log('Task 4');
+
+const employee = {};
+
+function addWorker() {
+    let countEmployee = 0;
+
+    class Worker {
+        #experience = 1.2;
+
+        constructor(fullName, dayRate, workingDays) {
+            this.fullName = fullName;
+            this.dayRate = dayRate;
+            this.workingDays = workingDays;
+        }
+
+        salary() {
+            let salary = this.dayRate * this.workingDays;
+            return salary;
+        }
+
+        showSalary() {
+            console.log(`${this.fullName} salary: ${this.salary()}`);
+        }
+
+        salaryWithExperience() {
+            let salaryExp = this.dayRate * this.workingDays * this.#experience;
+            return salaryExp;
+        }
+
+        showSalaryWithExperience() {
+            console.log(`${this.fullName} salary: ${this.salaryWithExperience()}`);
+        }
+
+        get showExperience() {
+            return this.#experience;
+        }
+
+        set setExperience(newExp) {
+            this.#experience = newExp;
+        }
+    }
+
+    return function (fullName, dayRate, workingDays) {
+        countEmployee++;
+
+        employee.length = countEmployee;
+
+        employee[`worker${countEmployee}`] = new Worker(
+            fullName,
+            dayRate,
+            workingDays
+        );
+
+    };
+
+}
+
+const newWorker = addWorker();
+
+newWorker('John Johnson', 20, 23);
+newWorker('Tom Tomson', 48, 22);
+newWorker('Andy Ander', 29, 23);
+
+console.log(employee.worker1.fullName);
+employee.worker1.showSalary();
+console.log('New experience: ' + employee.worker1.showExperience);
+employee.worker1.showSalaryWithExperience();
+employee.worker1.setExperience = 1.5;
+console.log('New experience: ' + employee.worker1.showExperience);
+employee.worker1.showSalaryWithExperience();
+
+console.log(employee.worker2.fullName);
+employee.worker2.showSalary();
+console.log('New experience: ' + employee.worker2.showExperience);
+employee.worker2.showSalaryWithExperience();
+employee.worker2.setExperience = 1.5;
+console.log('New experience: ' + employee.worker2.showExperience);
+employee.worker2.showSalaryWithExperience();
+
+console.log(employee.worker3.fullName);
+employee.worker3.showSalary();
+console.log('New experience: ' + employee.worker3.showExperience);
+employee.worker3.showSalaryWithExperience();
+employee.worker3.setExperience = 1.5;
+console.log('New experience: ' + employee.worker3.showExperience);
+employee.worker3.showSalaryWithExperience();
+
+function sortSalary(obj) {
+    const arr = [];
+
+    for (let i = 1; i <= obj.length; i++) {
+        let j = obj[`worker${i}`];
+        arr.push(j);
+    }
+
+    arr.sort((a, b) => {
+        return a.salaryWithExperience() - b.salaryWithExperience();
+    });
+
+    for (let k = 0; k < arr.length; k++) {
+        if (k === 0) {
+            console.log(`Sorted salary:
+  ${arr[k].fullName}: ${arr[k].salaryWithExperience()}`);
+        } else {
+            console.log(`${arr[k].fullName}: ${arr[k].salaryWithExperience()}`);
+        }
+    }
+
+    return arr;
+}
+sortSalary(employee);
+
+
+
+// task 5
+
+
+class GeometricFigure {
+    getArea() {
+    }
+    toString() {
+        return Object.getPrototypeOf(this).constructor.name;
+    }
+}
+
+class Triangle extends GeometricFigure {
+    constructor(base, height) {
+        super();
+        this.base = base;
+        this.height = height;
+    }
+    getArea() {
+        return this.base * this.height * 0.5;
+    }
+    toString() {
+        return Object.getPrototypeOf(this).constructor.name;
+    }
+}
+class Square extends GeometricFigure {
+    constructor(side) {
+        super();
+        this.side = side;
+    }
+    getArea() {
+        return this.side * this.side;
+    }
+    toString() {
+        return Object.getPrototypeOf(this).constructor.name;
+    }
+}
+
+class Circle extends GeometricFigure {
+    constructor(radius) {
+        super();
+        this.radius = radius;
+    }
+    getArea() {
+        return Math.PI * this.radius * this.radius;
+    }
+    toString() {
+        return Object.getPrototypeOf(this).constructor.name;
+    }
+}
+function handleFigures(figures) {
+    let totalFigArea = 0;
+    for (let i = 0; i < figures.length; i++) {
+        let figure = figures[i];
+        if (figure instanceof GeometricFigure) {
+            console.log(`Geometric figure: ${figure.toString()} - area: ${figure.getArea()}`);
+            totalFigArea += figure.getArea();
+        }
+    }
+    return totalFigArea;
+}
+const figures = [new Triangle(4, 5), new Square(7), new Circle(5)];
+console.log(handleFigures(figures));
 
